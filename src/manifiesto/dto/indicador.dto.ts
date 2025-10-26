f5d71a7394993806b5f8212e0e4141040d79a585
omar <ojramirezv@gmail.com>
2025-10-26 16:41:57 -0500

    se agregan dtos

---
+++ src/manifiesto/dto/indicador.dto.ts
@@ -0,0 +1,17 @@
+export class IndicadorDto {
+    rojo: string;
+    amarillo: string;
+    verde: string;
+    naranjo: string;
+    propuesta: string;
+    observacion: string;
+
+    constructor() {
+        this.rojo = '';
+        this.amarillo = '';
+        this.verde = '';
+        this.naranjo = '';
+        this.propuesta = '';
+        this.observacion = '';
+    }
+}
---
+++ src/manifiesto/services/clasificador-guias.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common';
+import { IndicadorDto } from '../dto/indicador.dto';
+
+@Injectable()
+export class ClasificadorGuiasService {
+    private readonly topeMaximo_Fiscalizable = 41.0;
+
+    clasificarGuia(
+        guia: any,
+        sumaValores: number,
+        sobreTopePorRut: number,
+        rutconsignatario: string
+    ): IndicadorDto {
+        const tipoRutConsignatario = guia.tipoRutConsignatario;
+        const indicadorColor = new IndicadorDto();
+        const valorLimite = this.topeMaximo_Fiscalizable;
+
+        if (tipoRutConsignatario === 'rutEmbajada') {
+            indicadorColor.observacion = 'RUTS_EMBAJADAS';
+            indicadorColor.verde = '<span class="color-box verde" title="Rut Embajada"></span>';
+            indicadorColor.propuesta = 'LIBRE';
+            return indicadorColor;
+        }
+
+        if (tipoRutConsignatario === 'rutBuzon') {
+            indicadorColor.observacion = 'rutBuzon';
+            indicadorColor.naranjo = '<span class="color-box naranjo" title="Rut Genérico"></span>';
+            indicadorColor.propuesta = 'RETENCIÓN';
+            return indicadorColor;
+        }
+
+        if (!rutconsignatario || rutconsignatario.trim() === '') {
+            indicadorColor.observacion = 'Obs: Rut consignatario no indicado en la guía..!!';
+            return indicadorColor;
+        }
+
+        if (tipoRutConsignatario === 'rutEmpresa') {
+            indicadorColor.observacion = 'rutEmpresa';
+            indicadorColor.amarillo = '<span class="color-box amarillo" title="Rut Empresa"></span>';
+            indicadorColor.propuesta = 'DECLARACIÓN';
+        }
+
+        if (sumaValores > valorLimite) {
+            indicadorColor.observacion = 'sobreTopeMaximo';
+            indicadorColor.naranjo = '<span class="color-box naranjo" title="Consignatario totaliza Guías >41 USD en este Manifiesto"></span>';
+            indicadorColor.propuesta = 'DECLARACIÓN';
+        }
+
+        if (sobreTopePorRut > 0) {
+            indicadorColor.observacion = 'tieneGuiasSobre41';
+            indicadorColor.rojo = '<span class="color-box rojo" title="Consignatario tiene Guías >41 USD en este Manifiesto"></span>';
+            indicadorColor.propuesta = 'DECLARACIÓN';
+        }
+
+        if (!indicadorColor.observacion || indicadorColor.observacion.trim() === '') {
+            indicadorColor.observacion = '-';
+            indicadorColor.propuesta = 'LIBRE';
+            indicadorColor.verde = '<span class="color-box verde" title="LIBRE"></span>';
+        }
+
+        return indicadorColor;
+    }
+}
